cc7a865fa57ff8cd921056a357926d8eebd4f0ba
Mahr <techmahr@gmail.com>
2026-03-17 18:08:35 +0530

    tailwind setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import toast from 'react-hot-toast';
 const App = () => {
   return (
     <div>
-      <button onClick={() => toast.success("Congrats")}>Click Me</button>
+      <button onClick={() => toast.success("Congrats")} className='text-blue-900'>Click Me</button>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/create' element={<CreatePage />} />
