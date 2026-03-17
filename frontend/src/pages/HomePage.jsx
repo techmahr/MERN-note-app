8ab567c9998e2a7b77515daee9589b7fae92ada1
Mahr <techmahr@gmail.com>
2026-03-17 23:33:35 +0530

    ratelimiter issue fixed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import { React, useState } from 'react'
 import Navbar from '../components/Navbar'
+import RateLimitedUI from '../components/RateLimitedUI';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 const HomePage = () => {
+
+    const [isRateLimited, setIsRateLimited] = useState(true);
+    const [notes, setNotes] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchNotes = async () => {
+            try {
+                // fetch api method 
+                // const res = await fetch("http://localhost:5001/api/notes");
+                // const data = await res.json();
+
+                // here we use axios 
+                const res = await axios.get("http://localhost:5001/api/notes")
+                console.log(res.data)
+
+            } catch (error) {
+                console.log("Error fetching notes", error)
+            }
+        };
+        fetchNotes();
+    }, []);
+
     return (
-        <div>
+        <div className='min-h-screen'>
             <Navbar />
+            {isRateLimited && <RateLimitedUI />}
         </div>
 
     )
