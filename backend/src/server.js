bf93925e7c3f273962e9a95f1b47153a397884cb
Mahr <techmahr@gmail.com>
2026-03-17 23:01:54 +0530

    cors added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors"
+import dotenv from "dotenv";
+
 import noteRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import ratelimiter from "./middleware/rateLimiter.js";
+
 
 dotenv.config();
 
@@ -18,9 +21,11 @@ app.use(express.json());
 
 app.use(ratelimiter) // middleware for limiting request to the application 
 
+app.use(cors({ // for cross browser data transfer issue fix 
+    origin: "http://localhost:5173" // cors will allow darta from any url, here we are restricting our api url 
+}));
+
 app.use("/api/notes", noteRoutes); // creates and api route and defines  end point http://localhost:5001/api/notes
-
-
 
 connectDB().then(() => { // connection to the DB
     // start the server 
