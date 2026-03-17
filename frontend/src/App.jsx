64655e776fd880371279166d8f1a6ff9c05cf43d
Mahr <techmahr@gmail.com>
2026-03-17 18:36:34 +0530

    daisy ui added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ const App = () => {
   return (
     <div>
       <button onClick={() => toast.success("Congrats")} className='text-blue-900'>Click Me</button>
+      <button className='btn btn-outline' >Click Me</button>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/create' element={<CreatePage />} />
