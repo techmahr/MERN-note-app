39230409bcb335a264f1a111eb41cb3d6c97cba1
Mahr <techmahr@gmail.com>
2026-03-17 23:54:34 +0530

    ratelimiter middleware order issue fixed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,10 +3,11 @@ import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI';
 import { useEffect } from 'react';
 import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const HomePage = () => {
 
-    const [isRateLimited, setIsRateLimited] = useState(true);
+    const [isRateLimited, setIsRateLimited] = useState(false);
     const [notes, setNotes] = useState([]);
     const [loading, setLoading] = useState(true);
 
@@ -18,11 +19,20 @@ const HomePage = () => {
                 // const data = await res.json();
 
                 // here we use axios 
-                const res = await axios.get("http://localhost:5001/api/notes")
-                console.log(res.data)
+                const res = await axios.get("http://localhost:5001/api/notes");  // this give full axios response object 
+                //console.log(res.data)
+                setNotes(res.data); // this stores actual note data 
+                setIsRateLimited(false); // if we are getting data, we are not rate limited 
 
             } catch (error) {
-                console.log("Error fetching notes", error)
+                console.log("Error fetching notes");
+                if (error.response.status === 429) {
+                    setIsRateLimited(true)
+                } else {
+                    toast.error("Failed to load notes")
+                }
+            } finally {
+                setLoading(false)
             }
         };
         fetchNotes();
