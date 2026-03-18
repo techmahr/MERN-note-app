c52419a6e25c6736de67e3e9f766ecf2d94e25e0
Mahr <techmahr@gmail.com>
2026-03-18 22:53:26 +0530

    no notes found updated

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import NoteCard from "../components/NoteCard";
 import { useEffect } from 'react';
 import toast from 'react-hot-toast';
 import api from "../lib/axios";
+import NotesNotFound from '../components/NotesNotFound';
 
 const HomePage = () => {
 
@@ -47,6 +48,8 @@ const HomePage = () => {
 
             <div className="max-w-7xl mx-auto p-4 mt-6">
                 {loading && <div className='text-AlignCenter text-primary py-10'>Loading notes...</div>}
+
+                {notes.length === 0 && !isRateLimited && <NotesNotFound />}
 
                 {notes.length > 0 && !isRateLimited && (
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
